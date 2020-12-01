6fef61ffe436952ec197198490f34e382891607a
xin-2020 <1713695034@qq.com>
2020-12-01 19:43:36 +0800

    在eclipse中向github发送文件

---
+++ EclipseProcjectPushToGithub/src/Main/EclipseJavaProjectPushToGithub.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class EclipseJavaProjectPushToGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello,github,this is eclipse");
+		
+	}
+
+}
